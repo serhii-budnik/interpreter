1b6f4c85e43b7b880124b7e248773aa4482e6746
Serhii Budnik <serhiib@scribd.com>
2023-09-12 12:14:28 +0300

    it's not needed

--- src/evaluator.rs
+++ src/evaluator.rs
@@ -1,18 +1,20 @@
-use crate::object::{ObjectType, Integer} ;
+use crate::object::ObjectType;
 
 pub fn eval<T>(node: T) -> ObjectType {
     // I think it should be enum instead of structs :/
-    ObjectType::Int(Integer { value: 5 })
+    ObjectType::Int(5)
 }
 
 #[cfg(test)]
 mod test {
     use super::eval;
+    use crate::object::ObjectType;
     use crate::ast::IntegerLiteral;
     use crate::token::Token;
 
     #[test]
     fn test_eval_of_integer() {
         let five = eval(IntegerLiteral { token: Token::Int("5".into()), value: 5 });
+        assert_eq!(five, ObjectType::Int(5))
     }
 }
--- src/object.rs
+++ src/object.rs
@@ -1,36 +1,6 @@
-use std::fmt::Display;
-
 #[derive(Debug, PartialEq)]
 pub enum ObjectType {
-    Int(Integer),
-    Bool(Boolean),
-    Null(Null),
-}
-
-pub struct Integer {
-    pub value: isize,
-}
-
-pub struct Boolean {
-    pub value: bool,
-}
-
-pub struct Null {}
-
-impl Display for Integer {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        write!(f, "{}", self.value)
-    }
-}
-
-impl Display for Boolean {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        write!(f, "{}", self.value)
-    }
-}
-
-impl Display for Null {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        write!(f, "null")
-    }
+    Int(isize),
+    Bool(bool),
+    Null,
 }
